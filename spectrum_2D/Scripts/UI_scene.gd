5d68bd3282eee506e6054f90b5cef29ed10ce94d
Brendan Wadey <62085968+Broondoon@users.noreply.github.com>
2024-03-16 17:12:01 -0600

    Small changes to UI

--- spectrum_2D/Scripts/UI_scene.gd
+++ spectrum_2D/Scripts/UI_scene.gd
@@ -1,6 +1,15 @@
 extends Control
 
 
+@onready var p_spec = $"HBoxContainer/PanelContainer/BaseSpectro/PianoSpectro"
+@onready var d_spec = $"HBoxContainer/PanelContainer/BaseSpectro/DrumSpectro"
+@onready var o_spec = $"HBoxContainer/PanelContainer/BaseSpectro/OtherSpectro"
+@onready var v_spec = $"HBoxContainer/PanelContainer/BaseSpectro/VoiceSpectro"
+
+# p_spec.visible = false
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +18,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+# boice
+func _on_check_box_toggled_1(toggled_on):
+	v_spec.visible = toggled_on
